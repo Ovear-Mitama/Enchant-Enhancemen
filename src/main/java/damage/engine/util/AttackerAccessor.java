ecfdb6841ecdb175bf049c2d7822fb3d22926bb5
mitama <zyrsky888@163.com>
2026-06-09 14:03:06 +0800

    删除 damage 文件夹

--- src/main/java/damage/engine/util/AttackerAccessor.java
+++
@@ -1,8 +0,0 @@
-package damage.engine.util;
-
-import net.minecraft.entity.Entity;
-import org.jetbrains.annotations.Nullable;
-
-public interface AttackerAccessor {
-    @Nullable Entity damageEngine$getAttacker();
-}
